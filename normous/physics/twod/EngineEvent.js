d46d3c7406ca3b0d49f09a3db8e2804b1bf11e8e
Norm McGarry <norm@byhook.com>
2013-07-03 08:18:45 -0400

    new tag

---
+++ normous/physics/twod/EngineEvent.js
@@ -0,0 +1,30 @@
+define([
+	'normous/events/Event'
+], function() {
+	
+	
+	Normous.namespace("Normous.Physics.Twod.EngineEvent");
+	
+	Normous.Physics.Twod.EngineEvent = function(config) {
+		this.bubbles = true;
+		this._super(config);
+	};
+	
+	Normous.Object.inherit(Normous.Physics.Twod.EngineEvent, Normous.Events.Event);
+	
+	Normous.Physics.Twod.EngineEvent.PARTICLE_ADDED = "particleAdded";
+	Normous.Physics.Twod.EngineEvent.PARTICLE_REMOVED = "particleRemoved";
+	Normous.Physics.Twod.EngineEvent.CONSTRAINT_ADDED = "constraintAdded";
+	Normous.Physics.Twod.EngineEvent.CONSTRAINT_REMOVED = "constraintRemoved";
+	Normous.Physics.Twod.EngineEvent.COMPOSITE_ADDED = "compositeAdded";
+	Normous.Physics.Twod.EngineEvent.COMPOSITE_REMOVED = "compositeRemoved";
+	Normous.Physics.Twod.EngineEvent.GROUP_ADDED = "groupAdded";
+	Normous.Physics.Twod.EngineEvent.GROUP_REMOVED = "groupRemoved";
+	
+	Normous.Physics.Twod.EngineEvent.prototype.element = null;
+	
+	Normous.Physics.Twod.EngineEvent.prototype.getElement = function() {
+		return this.element;
+	};
+	
+});
